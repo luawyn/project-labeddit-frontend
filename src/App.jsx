417864d6114830b732189b2fcd583ad5f8ad5dd1
Luana F Trevizani <92114917+luawyn@users.noreply.github.com>
2023-03-10 09:59:29 -0300

    loginPage finalizado

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { GlobalContext } from "./contexts/GlobalContext";
+import Router from "./routes/Router";
+
+function App() {
+  const context = {};
+
+  return (
+    <GlobalContext.Provider value={context}>
+      <Router />
+    </GlobalContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/routes/Router.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage/HomePage";
+import SignupPage from "../pages/SignupPage/SignupPage";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import PostPage from "../pages/PostPage/PostPage";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="signup" element={<SignupPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="post/:postId" element={<PostPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
